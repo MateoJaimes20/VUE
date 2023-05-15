54fb1a544a0af9d84a3b1a82ea6483a4daf4e6a7
SCIS3-PC17\SCIS3-17 <correo>
2023-05-15 13:07:43 -0500

    Tarjeta de notificaciones

--- reactividad/src/App.vue
+++ reactividad/src/App.vue
@@ -1,5 +1,29 @@
 <script setup>
 
+  import { ref } from "vue";
+
+  const notificacion = ref(0)
+
+  const incrementar = function () {
+    
+    
+    if (notificacion.value < 9) {
+      notificacion.value ++;
+    }else{
+      notificacion.value = 10 
+    }
+  }
+  const decrementar = () => {
+    notificacion.value --;
+
+    if (notificacion.value < 0) {
+      notificacion.value = 0;
+    }
+  }
+
+  
+
+
 </script>
 
 <template>
@@ -9,14 +33,14 @@
       <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
     </svg>
   
-    <div id="globito">
-      3
+    <div id="globito" v-if="notificacion > 0">
+      {{notificacion < 10  ? notificacion : "10+"}}
     </div>
 
     <br>
 
-    <spam class="buttom">+</spam>
-    <spam class="buttom">-</spam>
+    <spam @click='incrementar' class="buttom">+</spam>
+    <spam @click='decrementar' class="buttom">-</spam>
 
     
   </div>
