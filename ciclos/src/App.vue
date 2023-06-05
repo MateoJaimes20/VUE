8ba15117bb46f9d61764ebd91046a43daee3d980
Adrian Quintana <aequintanaa@ufpso.edu.co>
2023-06-05 13:03:18 -0500

    CICLOS

---
+++ ciclos/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import { ref } from 'vue';
+  import CardComponent from './components/CardComponent.vue';
+  const users = ref([
+    {
+      name:'Lionel Messi',
+      mail:'papadecris@mail.com'
+    },
+    {
+      name:'penaldo',
+      mail:'hijodelgoat@mail.com'
+    }
+  ])
+  const borrarTarjeta = (index) =>{
+    console.log('Matar a', index)
+    users.value.splice(index,1)
+    console.log(users)
+  }
+</script>
+
+<template>
+  
+  <h1 class="text-2xl">
+    Hola mundo
+  </h1>
+
+  <div class="space-y-2">
+    <CardComponent v-for="(user, index) in users" :key="index" :name="user.name" :mail="user.mail" :index="index" @borrarTarjeta="borrarTarjeta"/>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ ciclos/src/components/CardComponent.vue
@@ -0,0 +1,44 @@
+<script setup>
+    const mensaje = () =>{
+        console.log('Mensaje')
+    }
+
+    const borrarTarjeta = (index) => {
+        console.log('Borrar tarjeta',index)
+        emit('borrarTarjeta',index)
+    }
+    const emit = defineEmits(['borrarTarjeta'])
+
+    const props = defineProps(
+
+        { 
+            name:{
+                type: String,
+                default: 'no name'
+            },
+            mail:{
+                type: String,
+                default:'no mail'
+            },
+            index:{
+                type: Number
+            }
+        }
+    )
+</script>
+
+<template>
+    <div class="w-48 rounded shadow overflow-hidden m-5">
+        <div class="inline-block align-top" @click="mensaje">
+            <img src="https://picsum.photos/50" alt="">
+        </div>
+        <div class="inline-block pl-2">
+            <h2 class="text-md font-semibold leading-0">{{props.name}}</h2>
+            <p class="text-xs leading-0">{{props.mail}}</p>
+        </div>
+        <span class="inline-block ml-4 bg-red-700 text-white rounded py-0 px-2 m-2 hover:cursor-pointer hover:bg-red-900" @click="$event => borrarTarjeta(props.index)">
+        X
+        </span>
+    </div>
+    
+</template>
