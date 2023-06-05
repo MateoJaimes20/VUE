b657bfa2851c89c47caf01ae1d9b5e563df44476
Adrian Quintana <aequintanaa@ufpso.edu.co>
2023-06-05 13:14:08 -0500

    Ciclos

--- ciclos/src/App.vue
+++ ciclos/src/App.vue
@@ -16,6 +16,18 @@
     users.value.splice(index,1)
     console.log(users)
   }
+
+  let name, mail
+
+  const addUser = () => {
+    const newUser = {
+      name: name,
+      mail: mail
+    }
+
+    users.value.unshift(newUser)
+
+  }
 </script>
 
 <template>
@@ -23,6 +35,13 @@
   <h1 class="text-2xl">
     Hola mundo
   </h1>
+
+  <input type="text" v-model="name" placeholder="Nombre" class="borde rounded">
+  <br>
+  <br>
+  <input type="text" v-model="mail" placeholder="Mail" class="borde rounded">
+  <br>
+  <button class="bg-green-600 px-2 py-1 rounded mt-2" @click="addUser">Añadir</button>
 
   <div class="space-y-2">
     <CardComponent v-for="(user, index) in users" :key="index" :name="user.name" :mail="user.mail" :index="index" @borrarTarjeta="borrarTarjeta"/>
